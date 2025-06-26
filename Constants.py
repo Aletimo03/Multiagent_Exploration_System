d6b6373e483313fc0eb02ab67b5913dee675a0a5
Aletimo03 <alessio.timofte@stud.unifi.it>
2025-06-26 10:15:14 +0200

    changed a little agents heights

--- Constants.py
+++ Constants.py
@@ -6,8 +6,8 @@ AREA_LENGTH = 1000  # in meters
 NUM_OF_SAMPLES = 25  # number of points each agent generates as potentially new positions (default: 250, test: 25)
 EPSILON = 0.1  # percentage of how the agent moves in the chosen direction
 COMMUNICATION_RADIUS = 200  # of the agent (default: 200)
-DESIRED_COVERAGE_LEVEL = 10  # around 10dB Good coverage, video streaming
-MAX_DISPLACEMENT = 10 # max distance an agent can move from its actual position
+DESIRED_COVERAGE_LEVEL = 20  # around 10dB Good coverage, video streaming
+MAX_DISPLACEMENT = 10 #  max distance an agent can move from its actual position
 NUM_OF_ITERATIONS = 100 # max num of iterations before the algorithm stops (default: 100, test: 30)
 MIN_VERTICAL_DISTANCE = 0.15  # in meters
 SENSOR_HEIGHT = 0.15  # in meters
@@ -17,7 +17,7 @@ M = 30  # number of users
 N = 10  # number of agents
 B = 4  # number of base stations
 PENALTY = 1/M  # const for penalty search
-NUM_OF_SIMULATIONS = 12
+NUM_OF_SIMULATIONS = 20
 
 """ Power Spectral Density Noise """
 PSDN=4E-21 # Assuming 290K room temperature   PSDN(decibels relative to 1 milliwatt per Hertz)=-174dBm/Hz
@@ -33,7 +33,7 @@ CARRIER_FREQUENCY=2000
 TYPE_OF_SCENARIO="Urban"
 
 """Altitude of the sensors"""
-ALTITUDE = 50  # in meters
+ALTITUDE = 100  # in meters
 
 """Transmit Power"""
 TRANSMITTING_POWER = 0.2  # in Watts,  23dB
@@ -58,7 +58,7 @@ COUPLING_DISTANCE = EXPLORATION_CELL_WIDTH * 3
 INIT_PROBABLITY = 0.5
 
 # User movement — realistic human-like walking model
-USER_VELOCITY_MEAN = 2            # m/s — avg. walking speed (slightly reduced for smoother turns)
+USER_VELOCITY_MEAN = 2          # m/s — avg. walking speed (slightly reduced for smoother turns)
 USER_VELOCITY_STD = 0.1              # m/s — reduced variability for more stable paths
 USER_ANGULAR_VELOCITY_STD = 0.1     # rad/step — mild angular changes for ~1–2 turns per 30 steps
 
